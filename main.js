c1ffe74f6c28098f7d2bb503b17a6cfb0550f418
Ardi <ardiantosaputro2001@gmail.com>
2024-04-18 17:12:39 +0700

    Menuju ke preference-color-scheme

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
-const input = document.querySelector("input");
-const label = document.querySelector("label");
-const span = document.querySelector("span");
+// const input = document.querySelector("input");
+// const label = document.querySelector("label");
+// const span = document.querySelector("span");
+// const body = document.querySelector("body");
+// const sun = document.querySelector(".fa-sun");
+// const moon = document.querySelector(".fa-moon");
+// const heading = document.querySelector("h1");
+// const paragraph = document.querySelector("p");
 
-let isMoved = false;
+// let darkMode = (x) => {
+// 	span.style.transform = "translateX(41px)";
+// 	body.style.backgroundColor = "#36264c";
+// 	moon.classList.remove("icon-hidden");
+// 	sun.classList.add("icon-hidden");
+// 	heading.style.color = "#707875";
+// 	paragraph.style.color = "#707875";
+// 	label.style.borderColor = "#707875";
+// 	span.style.borderColor = "#707875";
+// };
 
-function toggleButton() {
-  if (!isMoved) {
-    span.style.transform = "translateX(41px)";
-  } else {
-    span.style.transform = "translateX(0px)";
-  }
-  isMoved = !isMoved;
-}
+// let lightMode = (x) => {
+// 	span.style.transform = "translateX(0px)";
+// 	body.style.backgroundColor = "#FFDF7E";
+// 	sun.classList.remove("icon-hidden");
+// 	moon.classList.add("icon-hidden");
+// 	heading.style.color = "#000";
+// 	paragraph.style.color = "#000";
+// 	span.style.backgroundColor = "#FFF";
+// };
 
-input.addEventListener("click", toggleButton);
+// let isMoved = false;
+
+// function toggleButton() {
+// 	if (!isMoved) {
+// 		darkMode();
+// 	} else {
+// 		lightMode();
+// 	}
+// 	isMoved = !isMoved;
+// }
+
+// input.addEventListener("click", toggleButton);
+
+
+const h2 = document.querySelector(".test");
+const classes = h2.classList;
+
+h2.addEventListener('click', e => {
+  const result = classes.toggle("New");
+  h2.textContent = `${result ? "added" : ""}
+  HUMUMUMUMU`;
+})
